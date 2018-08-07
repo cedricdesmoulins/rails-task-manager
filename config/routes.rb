9f395aed34aaddfd783fa6e81baa8fb5dc564817
cedric desmoulins <cdesmoulins@euromotion.care>
2018-08-07 17:46:58 +0200

    tasks

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,17 @@ Rails.application.routes.draw do
 #1. List tasks
 get "/tasks" => "tasks#index"
 
-#2. View Tasks
-get "/tasks/:id" => "tasks#show"
-
 #3. Add Tasks
-get "tasks/new" => "tasks#new"
+get "tasks/new" => "tasks#new", as: :new
 post "tasks" => "tasks#create"
 
+#2. View Tasks
+get "/tasks/:id" => "tasks#show", as: :task
+
 #4. Edit Tasks
-get "tasks/:id/edit", to: "tasks#edit"
+get "tasks/:id/edit", to: "tasks#edit", as: :edit
 patch "tasks/:id", to: "tasks#update"
 
 #5. Destroy Tasks
-delete "tasks/:id", to: "tasks#destroy"
+delete "tasks/:id", to: "tasks#destroy", as: :destroy
 end
