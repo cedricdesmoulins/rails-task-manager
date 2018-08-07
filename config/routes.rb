fc4afb158f027412155690b9bd9a35fdb1458a24
cedric desmoulins <cdesmoulins@euromotion.care>
2018-08-07 12:18:04 +0200

    routes creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#1. List tasks
+get "/tasks" => "tasks#index"
+
+#2. View Tasks
+get "/tasks/:id" => "tasks#show"
+
+#3. Add Tasks
+get "tasks/new" => "tasks#new"
+post "tasks" => "tasks#create"
+
+#4. Edit Tasks
+get "tasks/:id/edit", to: "tasks#edit"
+patch "tasks/:id", to: "tasks#update"
+
+#5. Destroy Tasks
+delete "tasks/:id", to: "tasks#destroy"
+
+
 end
